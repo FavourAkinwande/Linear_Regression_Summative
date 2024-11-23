87ff19e5f1c61400d614e6c24387a8a99b1ec05e
lola <f.akinwande@alustudent.com>
2024-11-23 18:28:28 +0200

    Initial commit for Flutter and FastAPI project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/prediction_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Waste Prediction',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: PredictionScreen(), 
+    );
+  }
+}
